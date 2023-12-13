37c644e1fb7e6dee67b9d8bdf16a2e4174b35621
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-12-13 21:44:24 +0200

    task11 fix

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -1,3 +1,3 @@
 -- 11. No table for a meeting
 
-CREATE VIEW need_meeting AS SELECT name FROM students WHERE score < 80;
+CREATE VIEW need_meeting AS SELECT name FROM students WHERE score < 80 AND (last_meeting IS NULL OR last_meeting < DATE_SUB(CURRENT_DATE(), INTERVAL 1 MONTH) ) ;
