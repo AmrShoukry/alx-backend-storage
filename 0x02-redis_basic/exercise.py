e6a76002f81d75c01a3ebb60d4c1309a02421d57
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-12-24 23:25:07 +0200

    task1

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -18,3 +18,20 @@ class Cache:
         random_key = str(uuid.uuid4())
         self._redis.set(random_key, data)
         return random_key
+
+    def get(self, key, fn):
+        """ get """
+        if not fn:
+            return self._redis.get(key)
+        if fn is int:
+            return self.get_int(key)
+        else:
+            return self.get_str(key)
+
+    def get_str(self, key):
+        """ get str """
+        return self._redis.get(key).decode('utf-8')
+
+    def get_int(self, key):
+        """ get int """
+        return int(self._redis.get(key).decode('utf-8'))
